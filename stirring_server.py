0f9f636626b99410e8e70b95e4d7a5ba338aa6c2
Gautam <113298649+Gautam1601@users.noreply.github.com>
2023-11-18 02:58:54 +0530

    stirring_server.py

--- stirring_server.py
+++ stirring_server.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from Movement_msgs.msg import StirringAction, StirringResult
+
+class StirringServer:
+    def __init__(self):
+        self.server = actionlib.SimpleActionServer('/stirring', StirringAction, self.execute, False)
+        self.server.start()
+
+    def execute(self, goal):
+        # Implement your stirring logic here
+        # ...
+
+        # Once the stirring is complete, set the result and finish the action
+        result = StirringResult()
+        result.success = True
+        self.server.set_succeeded(result)
+
+if __name__ == '__main__':
+    rospy.init_node('stirring_server')
+    stirring_server = StirringServer()
+    rospy.spin()
 
