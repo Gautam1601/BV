ed1b571c2d3514051541ea98316388482099e39a
Gautam <113298649+Gautam1601@users.noreply.github.com>
2023-11-18 03:18:35 +0530

    stirring_client.py

--- stirring_client.py
+++ stirring_client.py
@@ -9,13 +9,21 @@ class StirringClient:
         self.client = actionlib.SimpleActionClient('/stirring', StirringAction)
         self.client.wait_for_server()
 
-    def send_goal(self):
+    def send_goal(self, duration=5.0, direction=StirringGoal.CLOCKWISE, speed=1.0):
         goal = StirringGoal()
-        # Stirring Goal para
-        # Para Def baaki 
+        goal.duration = rospy.Duration(duration)
+        goal.direction = direction
+        goal.speed = speed
+
+        rospy.loginfo(f"Send stirring goal with duration={duration}s, direction={direction}, speed={speed}")
 
         self.client.send_goal(goal)
-        self.client.wait_for_result()
+
+        # Wait for the result
+        if self.client.wait_for_result(rospy.Duration(30.0)):
+            rospy.loginfo("Stirring completed successfully!")
+        else:
+            rospy.logwarn("Stirring did not complete within the specified time.")
 
 if __name__ == '__main__':
     rospy.init_node('stirring_client')
